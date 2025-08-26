9c363b70cc571146d6135d0d6926a2c6a1d6791a
Maxime Faucher <111804194+maximefaucher@users.noreply.github.com>
2025-08-25 20:40:53 -0400

    Ajout Cours 02

---
+++ Cours 02/Bonjour.java
@@ -0,0 +1,16 @@
+/*
+ * Auteur	: Maxime Faucher
+ * Date		: 2025/08/21
+ * Sujet	: Premier programme en Java
+ */
+
+public class Bonjour { // Début du code de la classe
+
+	public static void main(String[] args) { // Méthode principale, point d'entrée du programme
+
+		// Afficher un message en console
+		System.out.println("Bonjour le monde!");
+
+	}
+
+}
